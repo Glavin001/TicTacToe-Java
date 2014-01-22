f788bf94097137cf9b4d04fb31ae580f82c978c1
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-01-21 23:35:00 -0400

    Added old code for design of Engine.

--- ca.smu.cs.csci3465.assign1.GlavinWiechert/src/Engine.java
+++ ca.smu.cs.csci3465.assign1.GlavinWiechert/src/Engine.java
@@ -1,4 +1,32 @@
 
 public class Engine {
+	
+	/*
+	 * Variables
+	 */
+	public enum Player {
+	    PlayerX, 	// Player1.
+	    PlayerO, 	// Player2.
+	    PlayerNone // No player.
+	}
+	
+	/*
+	 * Methods
+	 */
+	public Player[][] getBoard() {
+		return null;
+	}
 
+	public Player getCurrentPlayer() {
+		return null;
+	}
+	
+	public Player[][] play(int[] position) {
+		return null;
+	}
+	
+	public Player getWinner() {
+		return null;
+	}
+	
 }
