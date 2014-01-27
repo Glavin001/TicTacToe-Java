060ea5884d4181a01994ca330e4a65bdf593a440
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-01-27 02:28:01 -0400

    GUIPlayer versus AIPlayer.

--- ca.smu.cs.csci3465.assign1.GlavinWiechert/src/App.java
+++ ca.smu.cs.csci3465.assign1.GlavinWiechert/src/App.java
@@ -15,16 +15,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         // Initialize instances.
-        Player human1 = new GUIPlayer();
-        Player human2 = new CommandLinePlayer();
-        Player computer1 = new RandomPlayer();
-        Player computer2 = new AIPlayer();
-        //Player computer2 = new AIPlayer();
+        Player firstPlayer = new GUIPlayer();
+        Player secondPlayer = new AIPlayer();
         Engine engine = new Engine();
         
         // Add Players to communicate to Engine and play the game.
-        engine.addPlayer(computer2);
-        engine.addPlayer(human1);
+        engine.addPlayer(firstPlayer); // First Player, 'X'
+        engine.addPlayer(secondPlayer); // Second Player, 'O'
         
         // Start the game!
         engine.startGame();
